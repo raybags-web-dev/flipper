0476b6435b6ee6cfaeb6b4bb9cabb2aca8b11031
Raymond <58218638+raybags-web-dev@users.noreply.github.com>
2021-08-06 10:28:02 +0200

    check
    check

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -5,7 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 set -e
-
+# strains
 main () {
   local -r THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
   ROOT_DIR=$(cd "$THIS_DIR" && hg root)
